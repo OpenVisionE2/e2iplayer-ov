2b362852df98cd003be287b04ddc7fe920dbfa93
Zdzislaw <zdzislaw22@windowslive.com>
2022-05-31 13:53:27 +0200

    ulepszenie instalatora

--- install-e2iplayer.sh
+++ install-e2iplayer.sh
@@ -42,13 +42,17 @@ else
 fi
 
 if [ -e /etc/opkg/opkg.conf ];then
-	[ $isPL -eq 1 ] && echo "Próbuję doinstalować brakujące pakiety z opkg" || echo "trying to install missing opkg packets"
-	opkg update > /dev/null 2>&1
-	opkg install python-html > /dev/null 2>&1
-	opkg install python-json > /dev/null 2>&1
-	[  $? -ne 0 ] && opkg install python-simplejson  > /dev/null 2>&1
-	opkg install python-compression > /dev/null 2>&1
-	opkg install openssl-bin > /dev/null 2>&1
+  [ $isPL -eq 1 ] && echo "Próbuję doinstalować brakujące pakiety z opkg" || echo "trying to install missing opkg packets"
+  opkg update > /dev/null 2>&1
+  opkg install python-html > /dev/null 2>&1
+  opkg install python-json > /dev/null 2>&1
+  [  $? -ne 0 ] && opkg install python-simplejson  > /dev/null 2>&1
+  opkg install python-compression > /dev/null 2>&1
+  opkg install openssl-bin > /dev/null 2>&1
+  [ `opkg list-installed|grep -c duktape` -eq 0 ] && opkg install duktape > /dev/null 2>&1
+  [ `opkg python3-e2icjson|grep -c python3-e2icjson` -eq 0 ] && opkg install python3-e2icjson > /dev/null 2>&1
+  [ `opkg python-e2icjson|grep -c python-e2icjson` -eq 0 ] && opkg install python-e2icjson > /dev/null 2>&1
+  
 fi
 [ $isPL -eq 1 ] && echo "KONIEC - przeładuj teraz E2" || echo "END - reload E2"
 sync
