0c5446a7e02653704a532bdf7fe6563c603b90b8
AYP <persianpros@yahoo.com>
2022-08-07 20:09:36 +0430

    Fix print_function for py2

--- IPTVPlayer/scripts/ustreamtv.py
+++ IPTVPlayer/scripts/ustreamtv.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from __future__ import print_function
 ###################################################
 #module run in different context then e2iplayer, must have separate version checking and assigments
 import sys
