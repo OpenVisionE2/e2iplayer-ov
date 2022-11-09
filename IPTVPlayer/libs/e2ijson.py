5fc1cfa64a67749a698fa1ca0b90c297ca4f6006
Zdzislaw <zdzislaw22@windowslive.com>
2022-11-09 07:50:42 +0100

    p2p3 > e2ijson stop using input keyword and better manage cjson failures

--- IPTVPlayer/libs/e2ijson.py
+++ IPTVPlayer/libs/e2ijson.py
@@ -17,7 +17,7 @@ e2icjson = None
 ############################################
 
 
-def loads(input, noneReplacement=None, baseTypesAsString=False, utf8=True):
+def loads(inputString, noneReplacement=None, baseTypesAsString=False, utf8=True):
     global e2icjson
     if e2icjson == None:
         if isPY2():
@@ -35,17 +35,22 @@ def loads(input, noneReplacement=None, baseTypesAsString=False, utf8=True):
 
     if e2icjson:
         printDBG(">> cjson ACELERATION noneReplacement[%s] baseTypesAsString[%s]" % (noneReplacement, baseTypesAsString))
-        out = e2icjson.decode(input, 2 if utf8 else 1)
-        if noneReplacement != None or baseTypesAsString != False:
-            printDBG(">> cjson ACELERATION byteify")
-            out = byteify(out, noneReplacement, baseTypesAsString)
+        try:
+            outString = e2icjson.decode(inputString, 2 if utf8 else 1)
+        except Exception as e:
+            # TEMPORARY: SHOULD BE DEEPLY INVESTIGATED why cjson sometimes fails but json not
+            printDBG(">> cjson FAILED with EXCEPTION: %s" % str(e))
+            printDBG("\t Problematic inputString = '%s'" % inputString)
+            printDBG(">> Trying with regular json module")
+            outString = json.loads(inputString)
     else:
-        out = json.loads(input)
-        if utf8 or noneReplacement != None or baseTypesAsString != False:
-            out = byteify(out, noneReplacement, baseTypesAsString)
+        outString = json.loads(inputString)
+    
+    if utf8 or noneReplacement != None or baseTypesAsString != False:
+        outString = byteify(outString, noneReplacement, baseTypesAsString)
 
-    return out
+    return outString
 
 
-def dumps(input, *args, **kwargs):
-    return json.dumps(input, *args, **kwargs)
+def dumps(inputString, *args, **kwargs):
+    return json.dumps(inputString, *args, **kwargs)
