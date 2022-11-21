e8f86fca5911781bceb235d7437c4c5cb20bede0
Zdzislaw <zdzislaw22@windowslive.com>
2022-11-21 18:18:41 +0100

    p3 > fix e2ijson

--- IPTVPlayer/libs/e2ijson.py
+++ IPTVPlayer/libs/e2ijson.py
@@ -13,12 +13,21 @@ try:
     import json
 except Exception:
     import simplejson as json
-e2icjson = None
+
+if isPY2():
+    e2icjson = None
+else: #e2icjson is not stable on py3 images, disabling is the best option for now
+    e2icjson = False
+
 ############################################
 
 
 def loads(inputString, noneReplacement=None, baseTypesAsString=False, utf8=True):
     global e2icjson
+
+    if not isPY2(): # PY3 works with utf8 so we can't use this flag with it
+        utf8 = False
+
     if e2icjson == None:
         if isPY2():
             try:
@@ -36,20 +45,20 @@ def loads(inputString, noneReplacement=None, baseTypesAsString=False, utf8=True)
     if e2icjson:
         printDBG(">> cjson ACELERATION noneReplacement[%s] baseTypesAsString[%s]" % (noneReplacement, baseTypesAsString))
         try:
-            outString = e2icjson.decode(inputString, 2 if utf8 else 1)
+            outDict = e2icjson.decode(inputString, 2 if utf8 else 1)
         except Exception as e:
             # TEMPORARY: SHOULD BE DEEPLY INVESTIGATED why cjson sometimes fails but json not
             printDBG(">> cjson FAILED with EXCEPTION: %s" % str(e))
             printDBG("\t Problematic inputString = '%s'" % inputString)
             printDBG(">> Trying with regular json module")
-            outString = json.loads(inputString)
+            outDict = json.loads(inputString)
     else:
-        outString = json.loads(inputString)
-
+        outDict = json.loads(inputString)
+    
     if utf8 or noneReplacement != None or baseTypesAsString != False:
-        outString = byteify(outString, noneReplacement, baseTypesAsString)
-
-    return outString
+        return byteify(outDict, noneReplacement, baseTypesAsString)
+    else:
+        return outDict
 
 
 def dumps(inputString, *args, **kwargs):
