4d0a9c9d29cff3eaadfd80fab8f2e50389a179b5
Zdzislaw <zdzislaw22@windowslive.com>
2022-12-26 08:54:52 +0100

    p3 > dalsze walki a modułami crypto

--- IPTVPlayer/libs/crypto/keyedHash/evp.py
+++ IPTVPlayer/libs/crypto/keyedHash/evp.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*-
 import sys
 
-if sys.version_info[0] == 3:
-  xrange = range
-
 def EVP_BytesToKey(md, data, salt, keyLength, ivLength, count):
-    assert (data)
-    assert (keyLength > 0)
-    assert (ivLength >= 0)
+    #below probably also works for py2, to check later when issue resolved
+    if sys.version_info[0] == 3:
+        dtot =  md(data + salt).digest()
+        d = [ dtot ]
+        while len(dtot)<(ivLength+keyLength):
+            d.append( md(d[-1] + data + salt).digest() )
+            dtot += d[-1]
+        return dtot[:keyLength], dtot[keyLength:keyLength+ivLength]
+
+    assert(data)
+    assert(keyLength > 0)
+    assert(ivLength >= 0)
     if salt:
-        assert (len(salt) == 8)
-    assert (count >= 1)
+        assert(len(salt) == 8)
+    assert(count >= 1)
 
     key = iv = hashed = ''
 
-    while True:
+    while 1:
         m = md()
         if hashed:
             m.update(hashed)
@@ -29,10 +35,8 @@ def EVP_BytesToKey(md, data, salt, keyLength, ivLength, count):
             hashed = m.digest()
 
         keyNeeds = keyLength - len(key)
-        if sys.version_info[0] == 2:
-            tmp = hashed
-        else:
-            tmp = hashed.decode('utf-8', 'ignore')
+        tmp = hashed
+
         if keyNeeds > 0:
             key += tmp[:keyNeeds]
             tmp = tmp[keyNeeds:]
