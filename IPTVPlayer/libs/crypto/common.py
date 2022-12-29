6bd7e9e179527c6dffdd5148b8f34c7dbbe1c466
Zdzislaw <zdzislaw22@windowslive.com>
2022-12-29 09:26:14 +0100

    mam nadzieję, że finał walki z anime

--- IPTVPlayer/libs/crypto/common.py
+++ IPTVPlayer/libs/crypto/common.py
@@ -5,6 +5,14 @@
     Copyright © (c) 2002 by Paul A. Lambert
     Read LICENSE.txt for license information.
 """
+import sys #required to catch PY2/PY3 and properly manage ord
+
+
+def getOrdForVal(val):
+    try:
+        return ord(val) #usually py2 and py3 for strings
+    except Exception:
+        return val #py3 for binaries
 
 
 def xorS(a, b):
@@ -12,7 +20,7 @@ def xorS(a, b):
     assert len(a) == len(b)
     x = []
     for i in range(len(a)):
-        x.append(chr(ord(a[i]) ^ ord(b[i])))
+            x.append(chr(getOrdForVal(a[i]) ^ getOrdForVal(b[i])))
     return ''.join(x)
 
 
@@ -20,5 +28,5 @@ def xor(a, b):
     """ XOR two strings """
     x = []
     for i in range(min(len(a), len(b))):
-        x.append(chr(ord(a[i]) ^ ord(b[i])))
+        x.append(chr(getOrdForVal(a[i]) ^ getOrdForVal(b[i])))
     return ''.join(x)
