oe-mirrors/e2iplayer-zadmario

add parser vtbe.net

IPTVPlayer/libs/urlparser.py
@@ -703,6 +703,7 @@ class urlparser:
                        'vshare.eu': self.pp.parserVSHAREEU,
                        'vshare.io': self.pp.parserVSHAREIO,
                        'vsports.pt': self.pp.parserVSPORTSPT,
+                       'vtbe.net': self.pp.parserONLYSTREAM,
                        'vtube.to': self.pp.parserONLYSTREAMTV,
                        'vup.to': self.pp.parserONLYSTREAMTV,
                        #w

OpenVisionE2/e2iplayer-ov

IPTVPlayer/libs/urlparser.py
@@ -703,6 +703,7 @@ class urlparser:
                        'vshare.eu': self.pp.parserVSHAREEU,
                        'vshare.io': self.pp.parserVSHAREIO,
                        'vsports.pt': self.pp.parserVSPORTSPT,
+                       'vtbe.net': self.pp.parserONLYSTREAMTV,
                        'vtube.to': self.pp.parserONLYSTREAMTV,
                        'vup.to': self.pp.parserONLYSTREAMTV,
                        #w